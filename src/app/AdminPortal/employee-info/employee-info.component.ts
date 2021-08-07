460000c6253caa3b714cc691ba40c68952e44ff0
staz6 <muhammad.aahad81@gmail.com>
2021-08-07 15:54:43 +0500

    attendance integeration done

--- src/app/AdminPortal/employee-info/employee-info.component.ts
+++ src/app/AdminPortal/employee-info/employee-info.component.ts
@@ -1,29 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Employee } from 'src/app/models/employee';
+import { EmployeesService } from 'src/app/services/employee/employees.service';
 import { LeavePopupComponent } from '../../AdminPortal/employee-info/leave-popup/leave-popup.component'
+import { AttendancePopupComponent } from './attendance-popup/attendance-popup.component';
 
 
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-employee-info',
@@ -32,15 +17,31 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class EmployeeInfoComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'action'];
-  dataSource = ELEMENT_DATA;
+  
+  displayedColumns: string[] = ['employee', 'designation', 'salary', 'contact', 'action'];
+  dataSource: Employee[]=[];
 
-  constructor( public dialog: MatDialog) {
+  constructor(private service:EmployeesService, public dialog: MatDialog) {
     
    }
 
-  ngOnInit(): void {}
-  openDialog() {
+  ngOnInit(): void {
+    this.getAllEmployeeInfo();
+  }
+
+  getAllEmployeeInfo(){
+    this.service.getAllEmployeeInfo().subscribe(response => {
+      this.dataSource=response;
+      console.log(this.dataSource);
+
+    })
+  }
+  openAttendanceDialog(userId:string){
+    const dialogRef=this.dialog.open(AttendancePopupComponent);
+    dialogRef.componentInstance.userId=userId;
+  }
+  openLeaveDialog() {
     const dialogRef = this.dialog.open(LeavePopupComponent);
-}
+    
+  }
 }
